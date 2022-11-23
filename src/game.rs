4b246da2bc656fcc0cc0a2a7287768e0a5cbfbc7
Paweł Zatoka <hoxiii@pm.me>
2022-11-23 15:35:43 +0100

    Add super basic version of WASM-based autosplitter

---
+++ src/game.rs
@@ -0,0 +1,180 @@
+use std::collections::HashSet;
+use asr::{
+    watcher::{Pair, Watcher},
+    Address, Process,
+};
+
+pub struct GameProcess {
+    pub process: Process,
+    pub state: State,
+    pub splits: HashSet<String>,
+}
+
+impl GameProcess {
+    pub fn connect(process_name: &str) -> Option<Self> {
+        let process = Process::attach(process_name)?;
+        let base_address = process.get_module_address("GTA5.exe").unwrap();
+
+        Some(Self {
+            process,
+            state: State::setup(base_address),
+            splits: HashSet::new(),
+        })
+    }
+}
+
+pub struct Variable<T> {
+    var: Watcher<T>,
+    base_address: Address,
+    address_path: Vec<u32>,
+}
+
+impl<T: bytemuck::Pod + std::fmt::Debug> Variable<T> {
+    pub fn update(&mut self, process: &Process) -> Option<&Pair<T>> {
+        self.var.update(
+            process
+                .read_pointer_path64(self.base_address.0.try_into().unwrap(), &self.address_path)
+                .ok(),
+        )
+    }
+}
+
+pub struct State {
+    pub stunt_jumps: Variable<i32>,
+    pub random_events: Variable<i32>,
+    pub hobbies: Variable<i32>,
+    pub current_cutscene: Variable<[u8; 100]>,
+    pub current_script: Variable<[u8; 100]>,
+    pub loading: Variable<i32>,
+    pub golf_hole: Variable<i32>,
+    pub in_cutscene: Variable<i8>,
+    pub in_mission: Variable<i8>,
+    pub in_mission2: Variable<i8>,
+    pub no_control: Variable<i8>,
+    pub debug_text: Variable<[u8; 100]>,
+    pub mpassed_screen: Variable<i32>,
+    pub collectible_screen: Variable<i32>,
+}
+
+impl State {
+    fn setup(base_address: Address) -> Self {
+        Self {
+            stunt_jumps: Variable {
+                var: Watcher::new(),
+                base_address,
+                address_path: vec![0x2A07E70, 0xCE5C0],
+            },
+            random_events: Variable {
+                var: Watcher::new(),
+                base_address,
+                address_path: vec![0x2A07E70, 0xBDA28],
+            },
+            hobbies: Variable {
+                var: Watcher::new(),
+                base_address,
+                address_path: vec![0x2A07E70, 0xBDA10],
+            },
+            current_cutscene: Variable {
+                var: Watcher::new(),
+                base_address,
+                address_path: vec![0x01CB44A0, 0xB70],
+            },
+            current_script: Variable {
+                var: Watcher::new(),
+                base_address,
+                address_path: vec![0x1CB4340],
+            },
+            loading: Variable {
+                var: Watcher::new(),
+                base_address,
+                address_path: vec![0x2AC7CF4],
+            },
+            golf_hole: Variable {
+                var: Watcher::new(),
+                base_address,
+                address_path: vec![0x1DE3970],
+            },
+            in_cutscene: Variable {
+                var: Watcher::new(),
+                base_address,
+                address_path: vec![0x1CB4472],
+            },
+            in_mission: Variable {
+                var: Watcher::new(),
+                base_address,
+                address_path: vec![0x1DD6CB9],
+            },
+            in_mission2: Variable {
+                var: Watcher::new(),
+                base_address,
+                address_path: vec![0x22959C3],
+            },
+            no_control: Variable {
+                var: Watcher::new(),
+                base_address,
+                address_path: vec![0x1DD034D],
+            },
+            debug_text: Variable {
+                var: Watcher::new(),
+                base_address,
+                address_path: vec![0x2295A10, 0x0],
+            },
+            mpassed_screen: Variable {
+                var: Watcher::new(),
+                base_address,
+                address_path: vec![0x2A07D48, 0xA60, 0x13C0],
+            },
+            collectible_screen: Variable {
+                var: Watcher::new(),
+                base_address,
+                address_path: vec![0x2AC7BA0, 0xD97A8],
+            },
+        }
+    }
+}
+
+impl State {
+    pub fn update(&mut self, process: &Process) -> Option<Variables> {
+        Some(Variables {
+            stunt_jumps: self.stunt_jumps.update(process)?,
+            random_events: self.random_events.update(process)?,
+            hobbies: self.hobbies.update(process)?,
+            current_cutscene: self.current_cutscene.update(process),
+            current_script: self.current_script.update(process),
+            loading: self.loading.update(process)?,
+            golf_hole: self.golf_hole.update(process)?,
+            in_cutscene: self.in_cutscene.update(process)?,
+            in_mission: self.in_mission.update(process)?,
+            in_mission2: self.in_mission2.update(process)?,
+            no_control: self.no_control.update(process)?,
+            debug_text: self.current_script.update(process),
+            mpassed_screen: self.mpassed_screen.update(process)?,
+            collectible_screen: self.collectible_screen.update(process)?,
+        })
+    }
+}
+
+pub struct Variables<'a> {
+    pub stunt_jumps: &'a Pair<i32>,
+    pub random_events: &'a Pair<i32>,
+    pub hobbies: &'a Pair<i32>,
+    pub current_cutscene: Option<&'a Pair<[u8; 100]>>,
+    pub current_script: Option<&'a Pair<[u8; 100]>>,
+    pub loading: &'a Pair<i32>,
+    pub golf_hole: &'a Pair<i32>,
+    pub in_cutscene: &'a Pair<i8>,
+    pub in_mission: &'a Pair<i8>,
+    pub in_mission2: &'a Pair<i8>,
+    pub no_control: &'a Pair<i8>,
+    pub debug_text: Option<&'a Pair<[u8; 100]>>,
+    pub mpassed_screen: &'a Pair<i32>,
+    pub collectible_screen: &'a Pair<i32>,
+}
+
+impl<'a> Variables<'a> {
+    pub fn get_as_string(var: &'a [u8]) -> Option<&'a str> {
+        let null_pos = var.iter().position(|&x| x == b'\0').unwrap_or(var.len());
+
+        std::str::from_utf8(&var[0..null_pos]).ok()
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+use asr::timer;
+use std::{collections::HashSet, sync::Mutex};
+
+pub mod game;
+use game::{GameProcess, Variables};
+
+static GAME_PROCESS: Mutex<Option<GameProcess>> = Mutex::new(None);
+
+#[no_mangle]
+pub extern "C" fn update() {
+    let mut mutex = GAME_PROCESS.lock().unwrap();
+    let mut game_open: bool = true; // Used to stop and resume game time on game launch/exit
+
+    if mutex.is_none() {
+        // (Re)connect to the game
+        *mutex = GameProcess::connect("GTA5");
+    } else {
+        let game = mutex.as_mut().unwrap();
+
+        // Make sure we're still connected to the game, pause game time if not
+        if !game.process.is_open() {
+            *mutex = None;
+            return;
+        }
+
+        let vars = match game.state.update(&mut game.process) {
+            Some(v) => v,
+            None => {
+                asr::print_message("Error updating state!");
+                return;
+            }
+        };
+
+        let splits = game.splits;
+
+        handle_split(&vars, &splits);
+        handle_start(&vars);
+    }
+}
+
+fn handle_split(vars: &Variables, splits: HashSet<String>) {
+    let mut current_golf_hole = 0;
+
+    // Stunt Jumps split
+    if (vars.stunt_jumps.current == vars.stunt_jumps.old + 1) {
+        timer::split();
+        asr::print_message("[Split] Stunt Jump");
+    }
+
+    // Random Event split
+    if (vars.random_events.current == vars.random_events.old + 1) {
+        timer::split();
+        asr::print_message("[Split] Random Event");
+    }
+
+    // Hobbies and Pastttimes split
+    if (vars.hobbies.current == vars.hobbies.old + 1) {
+        timer::split();
+        asr::print_message("[Split] Hobbies and Pasttimes");
+    }
+
+    // Ending A split
+    if (!splits.contains("Ending A Split")
+        && vars.no_control.current == 1
+        && vars.no_control.current != vars.no_control.old
+        && vars.in_mission.current == 1
+        && Variables::get_as_string(vars.current_cutscene.current).unwrap() == "fin_a_ext")
+    {
+        timer::split();
+        asr::print_message("[Split] Ending A split");
+        splits.insert("Ending A Split".to_owned());
+    }
+}
+
+fn handle_start(vars: &Variables) {
+    if (Variables::get_as_string(vars.debug_text.current).unwrap() == "PRO_SETTING"
+        && vars.debug_text.current != vars.debug_text.old)
+    {
+        asr::print_message("[Start] Starting timer due to Prologue start");
+        timer::start();
+    }
+}
